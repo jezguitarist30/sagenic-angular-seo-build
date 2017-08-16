8e8c98c474808f448b8dad72c975220151e591b4
jezguitarist30 <urban_jezreel09@yahoo.com>
2017-08-17 01:13:35 +0800

    enable prod mode in server.js and compile using es2015

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ const express = require('express');
 const compression = require('compression');
 const ngExpressEngine = require('@nguniversal/express-engine').ngExpressEngine;
 const renderModuleFactory = require('@angular/platform-server').renderModuleFactory;
-const app = express();
-const port = Number(process.env.PORT || 3000);
+const core = require('@angular/core');
 
 require('zone.js/dist/zone-node');
+
+core.enableProdMode();
+
+const app = express();
+const port = Number(process.env.PORT || 3000);
 
 var hash;
 fs.readdirSync(path.join(__dirname, 'server')).forEach(file => {
@@ -20,17 +24,17 @@ const AppServerModuleNgFactory = require('./server/main.' + hash + '.bundle').Ap
 
 const template = fs.readFileSync(path.join(__dirname, 'browser', 'index.html')).toString();
 
-app.engine('html', (_, options, callback) => {
+app.engine('.html', (_, options, callback) => {
   const opts = { document: template, url: options.req.url };
  
   renderModuleFactory(AppServerModuleNgFactory, opts)
     .then(html => callback(null, html));
  });
 
- 
+
+app.set('port', port);
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'browser'));
-
 
 // // console.log(path.join(__dirname, 'browser'));
 
@@ -39,12 +43,27 @@ app.use((req, res, next) => {
     next();
 });
 
-// // //app.use('/', express.static(path.join(__dirname, '/browser'), {index: false}));
-app.get(express.static(path.join(__dirname, 'browser')));
+function cacheControl(req, res, next) {
+  // instruct browser to revalidate in 60 seconds
+  res.header('Cache-Control', 'max-age=60');
+  next();
+}
 
-app.get('/*', (req, res) => {      
-  res.render('index', { req: req });
-});
+app.use('/', cacheControl, express.static(path.join(__dirname, 'browser'), {index: false, maxAge: 30}));
+//app.use('/',express.static(path.join(__dirname, 'browser'), { index: false }));
+
+function ngApp(req, res) {  
+  res.render('index', {
+    req,
+    res,
+    preboot: false, // turn on if using preboot
+    baseUrl: '/',
+    requestUrl: req.originalUrl,
+    originUrl: `http://localhost:${ app.get('port') }`
+  });
+}
+
+app.get('/', ngApp);
 
 // // // // app.get('/*', function (req, res) {
 // // // //   res.render('index', {

c5175f5936d25739eb5e325c56536fc2e7b118ae
jezguitarist30 <urban_jezreel09@yahoo.com>
2017-08-17 00:45:44 +0800

    updated server.js and package.json

--- server.js
+++ server.js
@@ -4,74 +4,54 @@ const express = require('express');
 const compression = require('compression');
 const ngExpressEngine = require('@nguniversal/express-engine').ngExpressEngine;
 const renderModuleFactory = require('@angular/platform-server').renderModuleFactory;
+const app = express();
+const port = Number(process.env.PORT || 3000);
 
 require('zone.js/dist/zone-node');
 
 var hash;
-fs.readdirSync(path.join(__dirname, '/server')).forEach(file => {
+fs.readdirSync(path.join(__dirname, 'server')).forEach(file => {
   if (file.startsWith('main')) {
-  hash = file.split('.')[1];
-}
+    hash = file.split('.')[1];
+  }
 });
 
 const AppServerModuleNgFactory = require('./server/main.' + hash + '.bundle').AppServerModuleNgFactory;
 
-const app = express();
-const port = Number(process.env.PORT || 3000);
+const template = fs.readFileSync(path.join(__dirname, 'browser', 'index.html')).toString();
 
-
-let template = fs.readFileSync(path.join(__dirname, '/browser', 'index.html')).toString();
-console.log(template);
 app.engine('html', (_, options, callback) => {
   const opts = { document: template, url: options.req.url };
-
+ 
   renderModuleFactory(AppServerModuleNgFactory, opts)
     .then(html => callback(null, html));
-});
+ });
 
-// // app.engine('html', ngExpressEngine({
-// //   baseUrl: 'http://localhost:' + port,
-// //   bootstrap: AppServerModuleNgFactory
-// // }));
-
-// // app.set('view engine', 'html');
-// // app.set('views', 'src')
-
-// // app.get('*.*', express.static(join(__dirname, '..', 'dist')));
-
-// // app.get('*', (req, res) => {
-// //   res.render('index', { req });
-// // });
-
-// // app.listen(PORT, () => {
-// //   console.log(`listening on http://localhost:${PORT}!`);
-// // });
-
+ 
 app.set('view engine', 'html');
-app.set('views', path.join(__dirname, '/browser'));
+app.set('views', path.join(__dirname, 'browser'));
 
-app.use(compression());
-//app.use('/', express.static(path.join(__dirname, '/browser'), {index: false}));
-app.get('*.*', express.static(path.join(__dirname, '/browser')));
 
-console.log(path.join(__dirname, '/browser'));
+// // console.log(path.join(__dirname, 'browser'));
 
 app.use((req, res, next) => {
     console.log('logging...');
     next();
 });
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-  //res.render('index', { req });
+// // //app.use('/', express.static(path.join(__dirname, '/browser'), {index: false}));
+app.get(express.static(path.join(__dirname, 'browser')));
+
+app.get('/*', (req, res) => {      
+  res.render('index', { req: req });
 });
 
-// // app.get('/*', function (req, res) {
-// //   res.render('index', {
-// //     req: req,
-// //     // res: res
-// //   });
-// // });
+// // // // app.get('/*', function (req, res) {
+// // // //   res.render('index', {
+// // // //     req: req,
+// // // //     // res: res
+// // // //   });
+// // // // });
 
 app.listen(port, function() {
   console.log(`Listening at ${port}`);
